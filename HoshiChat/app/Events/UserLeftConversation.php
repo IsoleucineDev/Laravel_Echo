3b962d14d01b4af322e7297639099bbe051e67ba
IsoleucineDev <a01773374@tec.mx>
2026-04-09 01:57:03 -0600

    feat: add broadcasting events for real-time chat

---
+++ HoshiChat/app/Events/UserLeftConversation.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Conversation;
+use App\Models\User;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class UserLeftConversation implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public User $user;
+    public Conversation $conversation;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(User $user, Conversation $conversation)
+    {
+        $this->user = $user;
+        $this->conversation = $conversation;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new Channel('conversation.' . $this->conversation->id),
+        ];
+    }
+
+    /**
+     * Get the data to broadcast.
+     */
+    public function broadcastWith(): array
+    {
+        return [
+            'user' => [
+                'id' => $this->user->id,
+                'name' => $this->user->name,
+            ],
+            'conversation_id' => $this->conversation->id,
+            'message' => $this->user->name . ' left the conversation',
+        ];
+    }
+}
