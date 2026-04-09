6e85fcfcc04a31d4a3f4a6e1b23552315994aa7e
IsoleucineDev <a01773374@tec.mx>
2026-04-09 02:28:09 -0600

    fix: add created_by field to conversation seeder

--- HoshiChat/database/seeders/ConversationSeeder.php
+++ HoshiChat/database/seeders/ConversationSeeder.php
@@ -21,29 +21,46 @@ class ConversationSeeder extends Seeder
         }
 
         // Crear conversaciones de prueba
+        $firstUser = $users->first();
+        
+        echo "\nCreando conversación 1 con created_by: " . $firstUser->id;
+        
         $conversation1 = Conversation::create([
             'name' => 'General Chat',
             'description' => 'General conversation for everyone',
+            'created_by' => $firstUser->id,
         ]);
 
         $conversation1->users()->attach($users->pluck('id')->toArray());
 
+        $secondUser = $users->skip(1)->first();
+        
+        echo "\nCreando conversación 2 con created_by: " . $secondUser->id;
+        
         $conversation2 = Conversation::create([
             'name' => 'Project Discussion',
             'description' => 'Discussion about the HoshiChat project',
+            'created_by' => $secondUser->id,
         ]);
 
         $conversation2->users()->attach(
             $users->take(3)->pluck('id')->toArray()
         );
 
+        $thirdUser = $users->skip(2)->first();
+        
+        echo "\nCreando conversación 3 con created_by: " . $thirdUser->id;
+        
         $conversation3 = Conversation::create([
             'name' => 'Random Topics',
             'description' => 'Talk about anything!',
+            'created_by' => $thirdUser->id,
         ]);
 
         $conversation3->users()->attach(
             $users->slice(1, 4)->pluck('id')->toArray()
         );
+        
+        echo "\nConversaciones creadas exitosamente!\n";
     }
 }
